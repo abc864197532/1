6d5d50dc6a723bb1b46d2915b9edee362a4e3c38
abc864197532 <65001169+abc864197532@users.noreply.github.com>
2021-02-15 20:11:44 +0800

    update running code

--- run/main2.cpp
+++ run/main2.cpp
@@ -651,7 +651,7 @@ int main() {
     int population = 40, number_of_change = 40, number_of_group = 5;
     tt = clock();
     cout << fixed << setprecision(10);
-    freopen("output.txt", "w", stdout);
+    freopen("output2.txt", "w", stdout);
     vector <vector <Player>> first(number_of_group), second(number_of_group);
     for (int i = 0; i < number_of_group; ++i) {
         for (int j = 0; j < population; ++j) {
